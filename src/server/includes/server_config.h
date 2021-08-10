3676900342f6c63ebe128fe1d8ac5b002c5bca05
LinkOut <luca.cirillo5@gmail.com>
2021-08-10 15:22:27 +0200

    add: includes folder; server config header

---
+++ src/server/includes/server_config.h
@@ -0,0 +1,26 @@
+#ifndef _SERVER_CONFIG_H
+#define _SERVER_CONFIG_H
+
+// Politica di rimpiazzamento
+enum POLICY { FIFO, LRU, LFU };
+
+// Percorso del file di configurazione specificato come parametro
+char* CONFIG_PATH;
+// Percorso di default per il file di configurazione
+const char* DEFAULT_CONFIG_PATH = "./config.txt";
+
+// Parametri di configurazione del Server
+// Numero di threads worker
+size_t THREADS_WORKER;
+// Numero massimo di file consentiti
+size_t STORAGE_MAX_FILES;
+// Dimensione massima dello Storage, in Mb
+size_t STORAGE_MAX_CAPACITY;
+// Politica di rimpiazzamento
+enum POLICY REPLACEMENT_POLICY;
+// Path al Socket file
+char* SOCKET_PATH;
+// Path al Log file
+char* LOG_PATH;
+
+#endif
