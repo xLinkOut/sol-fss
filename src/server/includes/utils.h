5f539066c72b96a633ed35c3d4ca42bdaf36e209
LinkOut <luca.cirillo5@gmail.com>
2021-09-25 11:39:08 +0200

    new: readn and writen implementation

--- src/server/includes/utils.h
+++ src/server/includes/utils.h
@@ -3,6 +3,8 @@
 #ifndef _UTIL_H
 #define _UTIL_H
 
+#include <stdlib.h>
+
 #define LOCK(lock)                               \
     if (pthread_mutex_lock(lock) != 0) {         \
         fprintf(stderr, "Error: LOCK failed\n"); \
@@ -32,5 +34,8 @@
         fprintf(stderr, "Error: BROADCAST failed\n"); \
         pthread_exit((void*)EXIT_FAILURE);            \
     }
+
+int readn(long fd, void* buf, size_t size);
+int writen(long fd, void* buf, size_t size);
 
 #endif
