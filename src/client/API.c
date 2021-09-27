e23fef0e28c74a1e42446afbdb14b7e76406bfea
LinkOut <luca.cirillo5@gmail.com>
2021-09-27 11:51:08 +0200

    chg: moved client APIs to separate file; minor changes

---
+++ src/client/API.c
@@ -0,0 +1,53 @@
+// @author Luca Cirillo (545480)
+
+#include <API.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+// Imposto il socket con un valore negativo, e.g. 'non connesso'
+int client_socket = -1;
+
+int openConnection(const char* sockname, int msec, const struct timespec abstime) {
+    // Controllo la validità degli argomenti
+    if (!sockname || msec < 0 || abstime.tv_sec < 0 || abstime.tv_nsec < 0) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    // Controllo che il client non abbia già instaurato una connessione al server
+    if (client_socket > -1) {
+        errno = EISCONN;
+        return -1;
+    }
+
+    struct sockaddr_un socket_address;
+    // Inizializzo la struttura
+    memset(&socket_address, '0', sizeof(socket_address));
+    socket_address.sun_family = AF_UNIX;
+    // Imposto come path quello del socket passato come argomento
+    // TODO: controlli su SOCKET_PATH; strncpy
+    strcpy(socket_address.sun_path, sockname);
+
+    // Creo il socket lato client, che si connetterà al server
+    client_socket = socket(AF_UNIX, SOCK_STREAM, 0);
+    // Controllo la buona riuscita dell'operazione
+    if (client_socket == -1) return -1;
+
+    // Struttura dati per la nanosleep
+    struct timespec sleep_time = {
+        .tv_sec = msec >= 1000 ? msec / 1000 : 0,    // Se l'attesa è maggiore di un secondo, uso proprio i secondi come misura
+        .tv_nsec = msec < 1000 ? msec * 1000000 : 0  // Altrimenti, uso i nanosecondi
+    };
+
+    int connect_status = -1;
+    // 'Aspetto e riprovo' finché la connessione non va a buon fine e non è ancora scaduto il timeout
+    while ((connect_status = connect(client_socket, (struct sockaddr*)&socket_address, sizeof(socket_address))) == -1 && time(NULL) < abstime.tv_sec) {
+        //printf("Connection failed, I'll try again ...\n");
+        nanosleep(&sleep_time, NULL);  // Aspetto per msec
+    }
+
+    if (connect_status == -1) errno = ETIMEDOUT;
+    return connect_status;
+}
---
+++ src/client/includes/API.h
@@ -0,0 +1,17 @@
+// @author Luca Cirillo (545480)
+
+#ifndef _API_H_
+#define _API_H_
+
+#include <time.h>
+
+// Socket del client
+int client_socket;
+
+// * Apre una connessione con il server al socket file sockname
+int openConnection(const char* sockname, int msec, const struct timespec abstime);
+
+// * Chiude la connessione associata al socket file sockname
+int closeConnection(const char* sockname);
+
+#endif
