3dbf0526b3a72e9a6d8aea45d8a7ab0cf7350c24
LinkOut <luca.cirillo5@gmail.com>
2021-09-29 15:55:19 +0200

    doc: more comments

--- src/server/storage.c
+++ src/server/storage.c
@@ -13,36 +13,47 @@ storage_t* storage_create(size_t buckets) {
         return NULL;
     }
 
+    // Alloco la memoria per lo storage
     storage_t* storage = malloc(sizeof(storage_t));
     if (!storage) return NULL;
+    // Creo la hashmap per memorizzare i files
     storage->files = icl_hash_create(buckets, NULL, NULL);
     if (!storage->files) {
         free(storage);
         return NULL;
     }
+
+    // Ritorno un puntatore allo storage
     return storage;
 }
 
 void storage_destroy(storage_t* storage) {
+    // Controllo la validità degli argomenti
     if (!storage) return;
+    // Cancello la hashmap
     icl_hash_destroy(storage->files, NULL, NULL);  // TODO: free key-data
+    // Libero la memoria dello storage
     free(storage);
 }
 
 storage_file_t* storage_file_create(const char* name, const void* contents, size_t size) {
+    // Controllo la validità degli argomenti
     if (!name) {
         errno = EINVAL;
         return NULL;
     }
+    // Alloco la memoria per un file
     storage_file_t* file = malloc(sizeof(storage_file_t));
     if (!file) return NULL;
 
+    // Salvo il nome del file
     if ((file->name = malloc(strlen(name) + 1)) == NULL) {
         free(file);
         return NULL;
     }
     strncpy(file->name, name, strlen(name) + 1);
 
+    // Salvo il contenuto del file
     if (contents && size != 0) {
         if ((file->contents = malloc(size)) == NULL) {
             free(file->name);
@@ -52,11 +63,15 @@ storage_file_t* storage_file_create(const char* name, const void* contents, size
         memcpy(file->contents, contents, size);
         file->size = size;
     }
+
+    // Ritorno un puntatore al file
     return file;
 }
 
 void storage_file_destroy(storage_file_t* file) {
+    // Controllo la validità degli argomenti
     if (!file) return;
+    // Libero la memoria occupata dal file
     free(file->name);
     free(file->contents);
     free(file);
