234c075fe239ecc593df5ddab0463e790fbf3536
LinkOut <luca.cirillo5@gmail.com>
2021-10-24 14:41:11 +0200

    add: test2

--- build/tests/test-2.sh
+++ build/tests/test-2.sh
@@ -7,40 +7,34 @@
 # *  con il flag -p attivo
 
 KILOBYTE=1024
-MEGABYTE=1048576
+MEGABYTE=1048576 # 1024 * 1024
 
 BUILD_DIR=./build
 TESTS_DIR=$BUILD_DIR/tests
 DUMMY_DIR=$TESTS_DIR/dummy
-SAVES_DIR=$DUMMY_DIR/saves
+SAVES_DIR=$TESTS_DIR/saves
 
-SOCKET_FILE=./fss.sk
+SOCKET_FILE=$BUILD_DIR/fss.sk
 CLIENT="$BUILD_DIR/client -f $SOCKET_FILE -p"
 
 # Genero alcuni dummy files per interagire sul server
 # Creo la cartella che conterrà i dummy file
-mkdir $DUMMY_DIR
+mkdir -p $DUMMY_DIR
 # Creo la cartella in cui ospitare eventuali file espulsi/letti
-mkdir $SAVES_DIR
+mkdir -p $SAVES_DIR
 # Genero 10 dummy file, da 100 a 1000 Kb
 for i in {1..10}; do
-    base64 /dev/urandom | head -c $(($i * $KILOBYTE * 100)) > $DUMMY_DIR/dummy-$i
+    base64 /dev/urandom | head -c $((($i * 100) * $KILOBYTE)) > $DUMMY_DIR/dummy-$i
 done
 
-# Testo tutte le operazioni implementate dal server, utilizzando più istanze client contemporaneamente
-for i in {1..10}; do
-    # -w: invio al server un'intera cartella, senza limite superiore al numero di file
-    # TODO
+# Per testare l'algoritmo di rimpiazzo, faccio molte scritture simultaneamente
+for i in {1..20}; do
     # -W: invio al server una lista di file specificati come argomento, e salvo eventuali file espulsi
-    $CLIENT -W $DUMMY_DIR/dummy-1,$DUMMY_DIR/dummy-2 -D $SAVES_DIR &
-    # -r: leggo dal server una lista di file
-    $CLIENT -r $DUMMY_DIR/dummy-1,$DUMMY_DIR/dummy-3 &
-    # -R: leggo dal server 5 file e li salvo 
-    $CLIENT -R n=5 -d $SAVES_DIR &
-    # -l: richiedo l'accesso in scrittura su due specifici file
-    $CLIENT -l $DUMMY_DIR/dummy-1,$DUMMY_DIR/dummy-2 &
-    # -d: rilascio l'accesso in scrittura su uno dei due file
-    $CLIENT -u $DUMMY_DIR/dummy-2 &
-    # -c: cancello il file su cui ho ancora l'accesso in scrittura
-    $CLIENT -c $DUMMY_DIR/dummy-1 &
+    $CLIENT -W $DUMMY_DIR/dummy-$((1 + $RANDOM % 10)),$DUMMY_DIR/dummy-$((1 + $RANDOM % 10)) -D $SAVES_DIR &
+    pids[${i}]=$!
+done
+
+# Aspetto che tutti i client abbiano finito prima di uscire
+for pid in ${pids[*]}; do
+    wait $pid
 done
